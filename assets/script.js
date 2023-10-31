b902053102a1844d9760e142d85832c169ecc5b0
hariomcuneiform <hariomcuneiform@gmail.com>
2023-10-31 12:04:37 +0530

    video button animated

--- assets/script.js
+++ assets/script.js
@@ -23,9 +23,9 @@ gsap.from(".introvideo", {
 gsap.from(".innerrocket", {
     scrollTrigger: {
         trigger: ".innerrocket",
-        scrub: true,
-        start: "top bottom",
-        end: "top top"
+        scrub: 3,
+        start: "500px bottom",
+        end: "bottom top"
     },
 
     xPercent: -800,
@@ -33,6 +33,39 @@ gsap.from(".innerrocket", {
     transformOrigin: "left right",
     ease: "none"
 });
+
+// video button movement 
+const videoContainer = document.getElementById('video-container');
+const video = document.getElementById('video');
+const playPauseButton = document.getElementById('play-pause-button');
+
+videoContainer.addEventListener('mouseenter', () => {
+    playPauseButton.style.display = 'grid';
+});
+
+videoContainer.addEventListener('mousemove', (e) => {
+    const x = e.clientX - videoContainer.getBoundingClientRect().left -25;
+    const y = e.clientY - videoContainer.getBoundingClientRect().top -35;
+    playPauseButton.style.left = `${x}px`;
+    playPauseButton.style.top = `${y}px`;
+});
+
+videoContainer.addEventListener('mouseleave', () => {
+    playPauseButton.style.display = 'none';
+});
+
+playPauseButton.addEventListener('click', () => {
+    if (video.paused) {
+        video.play();
+        playPauseButton.innerHTML = '<i class="fi fi-rr-pause"></i>';
+    } else {
+        video.pause();
+        playPauseButton.innerHTML = '<i class="fi fi-rr-play"></i>';
+    }
+});
+
+
+// testimonial section 
 
 var swiper = new Swiper(".revSwiper", {
     navigation: {
